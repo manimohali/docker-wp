45b8c0a0c8e6f21e14a0abab2004bb95626aef14
chandramani <chandramani@rudrainnovative.in>
2024-05-24 19:30:43 +0530

    Working docker

---
+++ wp-content/plugins/my-simple-plugin/my-simple-plugin.php
@@ -0,0 +1,12 @@
+<?php 
+/**
+ *  * Plugin Name: My Simple Plugin
+ * Plugin URI: https://example.com/my-simple-plugin
+ * Description: A simple plugin to demonstrate plugin development.
+ * Version: 1.0.0
+ * Author: Chandramani
+ * Author URI: https://example.com
+ * License: GPLv2 or later
+ * Text Domain: my-simple-plugin
+ * 
+ */
